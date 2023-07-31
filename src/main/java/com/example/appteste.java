99eafae206f87a2409f9c25372aa9fd61be10721
Gabriel Daim <daim117@hotmail.com>
2023-07-31 17:34:08 +0000

    atualizando estudos

--- src/main/java/com/example/appteste.java
+++ src/main/java/com/example/appteste.java
@@ -1,7 +1,24 @@
 package com.example;
 
+import java.util.Arrays;
+
 public class appteste {
     public static void main(String[] args) {
-        System.out.println("teste");
+        double vetorDouble[][] = new double[4][5];
+        for (int i = 0; i < vetorDouble.length; i++) {
+            for (int j = 0; j < vetorDouble[i].length; j++) {
+                vetorDouble[i][j] = 100 * (j+1) *(i+1);
+            }
+        }
+
+        for (int i = 0; i < vetorDouble.length; i++) {
+            for (int j = 0; j < vetorDouble[i].length; j++) {
+                System.out.printf("%6.1f ", vetorDouble[i][j]);
+            }
+            System.err.println();
+
+        }
+
+
     }
 }
